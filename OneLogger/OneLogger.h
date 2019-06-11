9afbb5ac00c6a2571f9196477fea3cc7df3bc6b9
Pranit-Harekar <pranitharekar@hotmail.com>
2019-06-10 20:21:17 -0400

    Initial Commit

---
+++ OneLogger/OneLogger.h
@@ -0,0 +1,19 @@
+//
+//  OneLogger.h
+//  OneLogger
+//
+//  Created by Pranit  Harekar on 6/10/19.
+//  Copyright © 2019 Pranit Harekar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OneLogger.
+FOUNDATION_EXPORT double OneLoggerVersionNumber;
+
+//! Project version string for OneLogger.
+FOUNDATION_EXPORT const unsigned char OneLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OneLogger/PublicHeader.h>
+
+
